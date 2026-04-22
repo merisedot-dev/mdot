905e1967c7b466925d0d694edec4d737e04886f9
kheldae <taho.gougeon@gmail.com>
2026-04-22 13:31:24 +0200

    structure for GtkListView

--- src/mdtree/mod.rs
+++ src/mdtree/mod.rs
@@ -5,5 +5,6 @@ use gtk::glib;
 glib::wrapper! {
     pub struct MDotGraphTree(ObjectSubclass<imp::MDotGraphTree>)
         @extends gtk::Box, gtk::Widget,
-        @implements gtk::Accessible, gtk::Actionable, gtk::Buildable, gtk::ConstraintTarget, gtk::Root;
+        @implements gtk::Accessible, gtk::Actionable, gtk::Buildable,
+                    gtk::ConstraintTarget, gtk::Root;
 }
